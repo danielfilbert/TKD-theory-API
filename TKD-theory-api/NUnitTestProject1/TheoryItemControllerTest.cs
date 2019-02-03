dab4c17e3ca4e76bce78b51c3e47d4dd09c1096f
Peter Meyer Elphick Frandsen <pemef@mhivestasoffshore.com>
2019-02-03 10:58:34 +0100

    Added get methods for korean and english theoryItems

--- TKD-theory-api/NUnitTestProject1/TheoryItemControllerTest.cs
+++ TKD-theory-api/NUnitTestProject1/TheoryItemControllerTest.cs
@@ -1,4 +1,5 @@
 using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
 using NUnit.Framework;
 using System.Collections.Generic;
 using TKDTheoryApi.Controllers;
@@ -9,6 +10,24 @@ namespace TKDTheoryApi.test
     [TestFixture]
     class TheoryItemControllerTest
     {
+        [Test]
+        public void GetKorTheoryItems200Test()
+        {
+            var context = A.Fake<ITKDTheoryAppContext>();
+            var seedData = new SeedData();
+            var korTheoryItems = new List<KorTheoryItem>
+            {
+                seedData.GetKorTheoryItem(1),
+                seedData.GetKorTheoryItem(2),
+                seedData.GetKorTheoryItem(3)
+            };
+            A.CallTo(() => context.GetKorTheoryItems()).Returns(korTheoryItems);
+            var controller = new TheoryItemController(context);
+
+            var result = controller.GetKorTheoryItems();
+            Assert.AreEqual(korTheoryItems, result.Value);
+        }
+
         [Test]
         public void GetDanTheoryItems200Test()
         {
@@ -25,6 +44,102 @@ namespace TKDTheoryApi.test
 
             var result = controller.GetDanTheoryItems();
             Assert.AreEqual(danTheoryItems, result.Value);
+        }
+
+        [Test]
+        public void GetEngTheoryItems200Test()
+        {
+            var context = A.Fake<ITKDTheoryAppContext>();
+            var seedData = new SeedData();
+            var engTheoryItems = new List<EngTheoryItem>
+            {
+                seedData.GetEngTheoryItem(1),
+                seedData.GetEngTheoryItem(2),
+                seedData.GetEngTheoryItem(3)
+            };
+            A.CallTo(() => context.GetEngTheoryItems()).Returns(engTheoryItems);
+            var controller = new TheoryItemController(context);
+
+            var result = controller.GetEngTheoryItems();
+            Assert.AreEqual(engTheoryItems, result.Value);
+        }
+
+        [Test]
+        public void GetKorTheoryItem200Test()
+        {
+            int id = 123;
+            var context = A.Fake<ITKDTheoryAppContext>();
+            var seedData = new SeedData();
+            var korTheoryItem = seedData.GetKorTheoryItem(id);
+            A.CallTo(() => context.GetKorTheoryItem(id)).Returns(korTheoryItem);
+            var controller = new TheoryItemController(context);
+
+            var result = controller.GetKorTheoryItem(id);
+            Assert.AreEqual(korTheoryItem, result.Value);
+        }
+
+        [Test]
+        public void GetKorTheoryItem404Test()
+        {
+            int id = 123;
+            var context = A.Fake<ITKDTheoryAppContext>();
+            A.CallTo(() => context.GetKorTheoryItem(id)).Returns(null);
+            var controller = new TheoryItemController(context);
+
+            var result = controller.GetKorTheoryItem(id);
+            Assert.IsInstanceOf<NotFoundResult>(result.Result);
+        }
+        
+        [Test]
+        public void GetDanTheoryItem200Test()
+        {
+            int id = 123;
+            var context = A.Fake<ITKDTheoryAppContext>();
+            var seedData = new SeedData();
+            var danTheoryItem = seedData.GetDanTheoryItem(id);
+            A.CallTo(() => context.GetDanTheoryItem(id)).Returns(danTheoryItem);
+            var controller = new TheoryItemController(context);
+
+            var result = controller.GetDanTheoryItem(id);
+            Assert.AreEqual(danTheoryItem, result.Value);
+        }
+
+        [Test]
+        public void GetDanTheoryItem404Test()
+        {
+            int id = 123;
+            var context = A.Fake<ITKDTheoryAppContext>();
+            A.CallTo(() => context.GetDanTheoryItem(id)).Returns(null);
+            var controller = new TheoryItemController(context);
+
+            var result = controller.GetDanTheoryItem(id);
+            Assert.IsInstanceOf<NotFoundResult>(result.Result);
+        }
+
+        [Test]
+        public void GetEngTheoryItem200Test()
+        {
+            int id = 123;
+            var context = A.Fake<ITKDTheoryAppContext>();
+            var seedData = new SeedData();
+            var engTheoryItem = seedData.GetEngTheoryItem(id);
+            A.CallTo(() => context.GetEngTheoryItem(id)).Returns(engTheoryItem);
+            var controller = new TheoryItemController(context);
+
+            var result = controller.GetEngTheoryItem(id);
+            Assert.AreEqual(engTheoryItem, result.Value);
+        }
+
+        [Test]
+        public void GetEngTheoryItem404Test()
+        {
+            int id = 123;
+            var context = A.Fake<ITKDTheoryAppContext>();
+            A.CallTo(() => context.GetEngTheoryItem(id)).Returns(null);
+            var controller = new TheoryItemController(context);
+
+            var result = controller.GetEngTheoryItem(id);
+            Assert.IsInstanceOf<NotFoundResult>(result.Result);
         }
     }
 }
--- TKD-theory-api/TKD-theory-api/Controllers/TheoryItemController.cs
+++ TKD-theory-api/TKD-theory-api/Controllers/TheoryItemController.cs
@@ -16,35 +16,71 @@ namespace TKDTheoryApi.Controllers
             _context = context;
         }
 
+        [Route("kor/")]
+        [HttpGet]
+        public ActionResult<IEnumerable<KorTheoryItem>> GetKorTheoryItems()
+        {
+            var korItems = _context.GetKorTheoryItems();
+            return new ActionResult<IEnumerable<KorTheoryItem>>(korItems);
+        }
+        
         [Route("dan/")]
         [HttpGet]
         public ActionResult<IEnumerable<DanTheoryItem>> GetDanTheoryItems()
         {
-            try
-            {
-                var danItems = _context.GetDanTheoryItems();
-                return new ActionResult<IEnumerable<DanTheoryItem>>(danItems);
-            }
-            catch (Exception exc)
-            {
-                throw exc;
-            }
-
+            var danItems = _context.GetDanTheoryItems();
+            return new ActionResult<IEnumerable<DanTheoryItem>>(danItems);
         }
 
+        [Route("eng/")]
+        [HttpGet]
+        public ActionResult<IEnumerable<EngTheoryItem>> GetEngTheoryItems()
+        {
+            var engItems = _context.GetEngTheoryItems();
+            return new ActionResult<IEnumerable<EngTheoryItem>>(engItems);
+        }
+
+        [Route("kor/{id}")]
+        [HttpGet]
+        public ActionResult<KorTheoryItem> GetKorTheoryItem(int id)
+        {
+            var korItem = _context.GetKorTheoryItem(id);
+            if(korItem == null)
+            {
+                return NotFound();
+            }
+            return new ActionResult<KorTheoryItem>(korItem);
+        }
+        
         [Route("dan/{id}")]
         [HttpGet]
         public ActionResult<DanTheoryItem> GetDanTheoryItem(int id)
         {
             var danItem = _context.GetDanTheoryItem(id);
-            return danItem;
+            if(danItem == null)
+            {
+                return NotFound();
+            }
+            return new ActionResult<DanTheoryItem>(danItem);
         }
 
-        [Route("dan/")]
-        [HttpPost]
-        public ActionResult<DanTheoryItem> PostDanTheoryItem()
+        [Route("eng/{id}")]
+        [HttpGet]
+        public ActionResult<EngTheoryItem> GetEngTheoryItem(int id)
         {
-            return null;
+            var engItem = _context.GetEngTheoryItem(id);
+            if(engItem == null)
+            {
+                return NotFound();
+            }
+            return new ActionResult<EngTheoryItem>(engItem);
         }
+
+        //[Route("dan/")]
+        //[HttpPost]
+        //public ActionResult<DanTheoryItem> PostDanTheoryItem()
+        //{
+        //    return null;
+        //}
     }
 }
